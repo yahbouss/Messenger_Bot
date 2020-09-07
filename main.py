e9d1f9589607cc6ffafdc118f4e4def80835ff0b
yahbouss <47297430+yahbouss@users.noreply.github.com>
2020-09-07 15:21:24 +0100

    Ver 0.1

---
+++ main.py
@@ -0,0 +1,48 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from LoginInfo import username, password
+
+
+class MyBot():
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+    def WebIn(self):
+        self.driver.get('https://www.messenger.com')
+
+        time.sleep(1)
+
+        fb_login = self.driver.find_element_by_xpath('//*[@id="email"]')
+        fb_login.send_keys(username)
+
+        fb_login_password = self.driver.find_element_by_xpath('//*[@id="pass"]')
+        fb_login_password.send_keys(password)
+
+        fb_login_click = self.driver.find_element_by_xpath('//*[@id="loginbutton"]')
+        fb_login_click.click()
+
+        fb_search = self.driver.find_element_by_xpath('//*[@id="js_u"]/div/div/div[1]/span[1]/label/input')
+        fb_search.send_keys('Yah Bouss')
+
+        time.sleep(1)
+
+        fb_target = self.driver.find_element_by_xpath('//*[@id="js_u"]/div/div/div[1]/span[1]/div/div/div[2]/ul/li/a/div/div[2]/div/div')
+        fb_target.click()
+
+        time.sleep(1)
+
+        fb_target_msgbox = self.driver.find_element_by_class_name('_5rpb')
+        #fb_target_msgbox.click()
+        time.sleep(1)
+        fb_target_msgbox_in = self.driver.find_element_by_class_name('notranslate')
+        fb_target_msgbox_in.send_keys('This is an automated messenger' + Keys.RETURN)
+        while True:
+            fb_target_msgbox_in.send_keys('How you doin' + Keys.RETURN)
+            print('message (sent)')
+            time.sleep(0.25)
+
+
+
+bot = MyBot()
+
+bot.WebIn()
